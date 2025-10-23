65b90bc4f5632f7f8bf4f689df1cea39d4a82704
Rob Walker <66529029+robwalker-codes@users.noreply.github.com>
2025-10-23 12:37:57 +0100

    Fix IDE capability and build integration

--- ide/src-tauri/src/main.rs
+++ ide/src-tauri/src/main.rs
@@ -174,6 +174,8 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_window_state::Builder::default().build())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
             open_db,
             exec_sql,
